350e56ac48efb88eb1ad7f8a640689b0c00278ad
Mr. Is <isaac.ardis@gmail.com>
2016-06-14 09:14:40 -0400

    seeds ahoy with tender loving comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,109 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   
+
+# How many friends do you have?
+# 1:1 :: Characters:Guests
+PARTY_SIZE = 5
+
+# Create a host. 
+# Host is like User. They must register/sign in with email and password. So authentication will be required to host games, but not to be a Guest.
+host = Host.create({name: Faker::StarWars.character, email: 'murder@mystery.kit', password: 'asdfasdf', password_confirmation: 'asdfasdf'})
+
+# Create games.
+# 
+# The game of the evening.
+tonights_game = Game.create({
+		name: 'Scarlet O\'Scarya',
+		backstory: 'A russian mistress with a passion for passion.'
+	})
+another_random_game = Game.create({
+		name: 'A Night at the Zoo', 
+		backstory: 'Whoo done it?'
+	})
+
+# Create some special characters for tonight's game. 
+# See how it's creating multiple records at once with an Array?
+tonights_game.characters.create([
+	{
+		name: 'Fraulein Spizz',
+		backstory: 'From a long long way away...',
+		attire: 'Red hat, white gloves, hot temper.'
+	},
+	{
+		name: 'Herr Spiegelbald',
+		backstory: 'An trusty crook caught in double jeapordy.',
+		attire: 'Ivory cane and a pet mouse.'
+	}
+])
+# add 3 more random characters to tonight's game so we have matching number of guests and characters. 
+(PARTY_SIZE - 2).times do |t|
+	tonights_game.characters.create({
+		name: Faker::Superhero.name,
+		backstory: Faker::Lorem.sentence,
+		attire: Faker::Commerce.product_name
+		})
+end
+
+# create more characters for second game for mass appeal
+4.times do |t|
+	another_random_game.characters.create({
+		name: Faker::Name.name,
+		backstory: Faker::Lorem.sentence,
+		attire: Faker::Lorem.sentence
+		})
+end
+
+# Create events for first game.
+# Hans Solo smashes the chandelier with a hockey stick on accident.
+game_events = 8.times do |t|
+
+	accidental_chandelier_smashing = tonights_game.events.create({
+		title: Faker::StarWars.quote,
+		chronology: t
+		})
+
+	# involve a random (>=0) number of characters in each event
+	available_characters = tonights_game.characters.all.to_a
+	# rand => 0.2345342, ie random 0-1 float
+	# round => rounds up to nearest Integer
+	(rand * (PARTY_SIZE)).round.times do |t|
+		# Array.shift returns and removes item from array
+		accidental_chandelier_smashing.event_characters.create(character_id: available_characters.shift.id)
+	end
+end
+
+# Let's party. 
+
+# Create an evening with our first game for our skywalking host.
+evening = host.evenings.create({
+	game_id: tonights_game.id,
+	location: Faker::Address.street_address,
+	caveat: Faker::Hipster.sentence
+	})
+
+# Invite some friends to our special evening.
+PARTY_SIZE.times do |t|
+	
+	# Guests' name as var to make available for reuse in email.
+	name = Faker::Name.name
+	
+	evening.guests.create({
+		name: name,
+		phone_number: Faker::PhoneNumber.cell_phone,
+		 # Use just first name for email.
+		 # String.split('x') => Array delimited by x
+		 # [0] => first object in array, ie first name
+		email: Faker::Internet.free_email(name.split(' ')[0])
+		})
+end
+
+# Assign each of our guests to characters in the game.
+evening.guests.each do |guest|
+	guest.guest_characters.create({
+		evening_id: evening.id,
+		character_id: evening.game.characters.to_a.shift.id # to_a (like above) cuz it must be an Array, not an ActiveRecord::Association
+		})
+end
+
